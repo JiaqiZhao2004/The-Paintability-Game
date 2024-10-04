1a9c61422236638401abdef9c423066ddcdc5e66
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-10-04 14:49:36 -0500

    made StartPage configurable with arguments

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,13 +1,20 @@
 // import { useState } from 'react'
 
-import StartPage from './pages/StartPage'
+import StartPage from "./pages/StartPage";
+import ReactLogo from "./assets/react.svg"; // adjust the path according to your file structure
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    StartPage()
-  )
+    <StartPage
+      title="The Paintability Game"
+      description="A single-player strategy game based on graph theory."
+      onClickTutorial={() => null}
+      onClickPlay={() => null}
+      image={ReactLogo}
+    />
+  );
 }
 
-export default App
+export default App;
--- frontend-react/src/pages/StartPage.tsx
+++ frontend-react/src/pages/StartPage.tsx
@@ -1,30 +1,37 @@
-import ReactLogo from "../assets/react.svg"; // adjust the path according to your file structure
+interface Props {
+    title: string;
+    description: string;
+    onClickTutorial: () => any;
+    onClickPlay: () => any;
+    image: string | undefined;
+}
 
-const StartPage = () => {
+const StartPage = ({title, description, onClickTutorial, onClickPlay, image=undefined} : Props) => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="text-center w-100">
         <img
           className="d-block mx-auto mb-4"
-          src={ReactLogo}
+          src={image}
           alt=""
           width="72"
           height="57"
         />
         <h1 className="display-5 fw-bold text-body-emphasis">
-          The Paintability Game
+          {title}
         </h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
-            A single-player strategy game based on graph theory.
+            {description}
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3" onClick={onClickPlay}>
               Play
             </button>
             <button
               type="button"
               className="btn btn-outline-secondary btn-lg px-4"
+              onClick={onClickTutorial}
             >
               Tutorial
             </button>
