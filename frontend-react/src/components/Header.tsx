ebac54ac7bbc474eb19da24365fb40d4c3d12e40
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-11-17 16:51:00 -0600

    replaced a tags with Link from react-router-dom

--- frontend-react/src/components/Header.tsx
+++ frontend-react/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 interface Props {
 	title: string;
@@ -15,28 +16,28 @@ const Header = ( {title, items, redirects, selectedIndex, image = undefined} : P
 		style ={{height: "10vh",}}
 		>
 			<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-				<a
-					href=""
+				<Link
+					to="/home"
 					className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
 				>
 					<svg className="bi me-2" width="40" height="32">
 						{image}
 					</svg>
 					<span className="fs-4">{title}</span>
-				</a>
+				</Link>
 
 				<ul className="nav nav-pills">
 					{items.map((item, index) => (
 						<li key={index} className="nav-item">
-							<a
-								href={'/' + redirects[index]}
+							<Link
+								to={'/' + redirects[index]}
 								className={
 									selectedIndex === index ? "nav-link active" : "nav-link"
 								}
 								aria-current="page"
 							>
 								{item}
-							</a>
+							</Link>
 						</li>
 					))}
 				</ul>
