e6dfef5c06846a73777575b42a14a7a06e25d299
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-11-23 09:31:37 -0600

    added redirect to /play when clicking any Card in RolePage

--- frontend-react/src/pages/RolePage.tsx
+++ frontend-react/src/pages/RolePage.tsx
@@ -126,7 +126,9 @@ const RolePage = ({
 			<div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
 				{cards.map((item, index) => (
 					<div className="col" key={index}>
-						<Card {...item} />
+						<a href="/play">
+							<Card {...item} />
+						</a>
 					</div>
 				))}
 			</div>
