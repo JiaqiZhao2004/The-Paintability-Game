5a61ef5f9b15116a518b31fef8e81aa4aafca403
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-12-12 16:46:59 -0600

    updated layout of Card component

--- frontend-react/src/components/Card.tsx
+++ frontend-react/src/components/Card.tsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 interface Props {
 	title: string;
 	description: string;
@@ -7,14 +9,21 @@ interface Props {
 
 const Card = ({ title, description, backgroundImg, onClick }: Props) => {
 	return (
-		<div
-			className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-			style={{ backgroundImage: `url(${backgroundImg})` }}
-			onClick={onClick}
-		>
-			<div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-				<h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{title}</h3>
-				<p>{description}</p>
+		<div className="card card-cover h-100 overflow-hidden text-bg-dark shadow-lg">
+			<div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-4">
+				<img src={backgroundImg}></img>
+				<h3 className="h-100 pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{title}</h3>
+				<p className="h-50">{description}</p>
+			</div>
+			<div style={{ padding: 50 }}>
+				<Button
+					label="Select"
+					color="warning"
+					onClick={onClick}
+					heightPctg={2}
+					widthPctg={5}
+					fontSize={22}
+				/>
 			</div>
 		</div>
 	);
