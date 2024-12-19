a354087c8bb5d06856a1d11d7094a5245aa73039
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-12-19 02:03:34 -0600

    enlarged image in Hero and changed text color

--- frontend-react/src/components/Hero.tsx
+++ frontend-react/src/components/Hero.tsx
@@ -7,13 +7,20 @@ interface Props {
 const Hero = ({ title, description, image }: Props) => {
 	return (
 		<>
-			{image && <img
-				className="d-block mx-auto mb-4"
-				src={image}
-				alt="game icon"
-				width="110"
-			/>}
-			<h1 className="display-5 fw-bold text-body-emphasis">{title}</h1>
+			{image && (
+				<img
+					className="d-block mx-auto mb-4"
+					src={image}
+					alt="game icon"
+					width="22%"
+				/>
+			)}
+			<h1
+				className="display-5 fw-bold text-body-emphasis"
+				style={{ WebkitTextFillColor: "#1f1f1f" }}
+			>
+				{title}
+			</h1>
 			<div className="col-lg-6 mx-auto">
 				<p className="lead mb-4">{description}</p>
 			</div>
