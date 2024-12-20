a360ba8c0dbeb67d0e88cdd540ab1a9582cbba1e
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-12-19 21:34:02 -0600

    created CSS for Header component

--- frontend-react/src/components/Header.tsx
+++ frontend-react/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import FortressImg from "../assets/fortress.png";
+import "./Header.css";
 
 interface Props {
 	title: string;
@@ -8,41 +10,36 @@ interface Props {
 	image?: string;
 }
 
-const Header = ( {title, items, redirects, selectedIndex, image = undefined} : Props) => {
-
-
+const Header = ({
+	title,
+	items,
+	redirects,
+	selectedIndex,
+	image = FortressImg,
+}: Props) => {
 	return (
-		<div className="container"
-		style ={{height: "10vh",}}
-		>
-			<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-				<Link
-					to="/home"
-					className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-				>
-					<svg className="bi me-2" width="40" height="32">
-						{image}
-					</svg>
-					<span className="fs-4">{title}</span>
-				</Link>
+		<header className="header">
+			<Link className="logo" to="/home">
+				<img src={image} />
+				<h2>{title}</h2>
+			</Link>
 
-				<ul className="nav nav-pills">
-					{items.map((item, index) => (
-						<li key={index} className="nav-item">
-							<Link
-								to={'/' + redirects[index]}
-								className={
-									selectedIndex === index ? "nav-link active" : "nav-link"
-								}
-								aria-current="page"
-							>
-								{item}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</header>
-		</div>
+			<ul className="nav nav-pills custom-nav">
+				{items.map((item, index) => (
+					<li key={index}>
+						<Link
+							to={selectedIndex === index ? "" : "/" + redirects[index]} // no redirect if select same page
+							className={
+								selectedIndex === index ? "custom-active" : "custom-inactive"
+							}
+							aria-current="page"
+						>
+							{item}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</header>
 	);
 };
 export default Header;
